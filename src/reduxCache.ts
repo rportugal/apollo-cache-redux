f46f6276f296e5ba96d8e4555374442f4bc8b4c8
Ricardo Portugal <ricardofmportugal@gmail.com>
2017-12-09 21:02:13 +0000

    Reduce number of write actions (#11)
    * one arg with all options
    
    * improve readme; bump version
    
    * version bump

--- src/reduxCache.ts
+++ src/reduxCache.ts
@@ -1,12 +1,17 @@
 import {
+    Cache,
+} from 'apollo-cache';
+import {
     ApolloReducerConfig,
     InMemoryCache,
+    writeResultToStore
 } from 'apollo-cache-inmemory';
 
 import {
     ReduxNormalizedCacheConfig,
     reduxNormalizedCacheFactory
 } from './reduxNormalizedCache';
+import {cloneDeep} from 'lodash';
 
 export type ReduxCacheConfig = ApolloReducerConfig & ReduxNormalizedCacheConfig;
 
@@ -15,5 +20,23 @@ export class ReduxCache extends InMemoryCache {
         super(config);
         // Overwrite the in-memory data object
         this.data = reduxNormalizedCacheFactory({}, config);
+    }
+
+    public write(write: Cache.WriteOptions): void {
+        const data = this.config.storeFactory(cloneDeep(this.data.toObject()));
+
+        writeResultToStore({
+            dataId: write.dataId,
+            result: write.result,
+            variables: write.variables,
+            document: this.transformDocument(write.query),
+            store: data,
+            dataIdFromObject: this.config.dataIdFromObject,
+            fragmentMatcherFunction: this.config.fragmentMatcher.match,
+        });
+
+
+        this.data.replace(data.toObject());
+        this.broadcastWatches();
     }
 }
