c1b37238aa6f485eb238e47f534fbf3a1759f9bf
Ricardo Portugal <ricardofmportugal@gmail.com>
2018-09-23 17:42:53 +0100

    Update dependencies (#28)
    Update dependencies (Apollo cache)
    Add Redux 4 as peer dependency
    Add simple Node examples with Redux 3 and 4 to show state

---
+++ examples/redux4/src/index.js
@@ -0,0 +1,60 @@
+import { ReduxCache, apolloReducer } from 'apollo-cache-redux';
+import gql from 'graphql-tag';
+import 'cross-fetch/polyfill';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import ApolloClient from 'apollo-boost';
+import logger from 'redux-logger';
+
+const GRAPHQL_ENDPOINT = 'https://graphql-pokemon.now.sh';
+const EXAMPLE_QUERY = gql`
+  {
+    pokemon(name: "Pikachu") {
+      id
+      number
+      name
+      attacks {
+        special {
+          name
+          type
+          damage
+        }
+      }
+      evolutions {
+        id
+        number
+        name
+        weight {
+          minimum
+          maximum
+        }
+        attacks {
+          fast {
+            name
+            type
+            damage
+          }
+        }
+      }
+    }
+  }
+`;
+const store = createStore(
+  combineReducers({
+    apollo: apolloReducer
+  }),
+  applyMiddleware(logger)
+);
+
+store.subscribe(() => {});
+const cache = new ReduxCache({ store });
+
+const client = new ApolloClient({
+  uri: GRAPHQL_ENDPOINT,
+  cache
+});
+
+client.query({ query: EXAMPLE_QUERY }).then(() => {
+  console.log('#################');
+  console.log('-> Current state:');
+  console.log(store.getState());
+});
